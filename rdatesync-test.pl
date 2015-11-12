f811dbf066114eaa258bd0ffa0757653fe2cb348
Paul Walker <wpaulsg@gmail.com>
2015-11-11 21:05:56 -0600

    rdatesync: test for correctly-read configs

--- rdatesync-test.pl
+++ rdatesync-test.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-use Test::Simple tests => 1;
+use Test::Simple tests => 3;
 
 my $RDATESYNC = `printf \$(cd \$(dirname $0) && pwd)/rdatesync.pl`;
 
@@ -9,4 +9,22 @@ sub PrintUsageNoArgs {
 	ok( $output =~ "Usage" );
 }
 
+# TestBasicConfig - test that rdatesync.pl can read a config file
+sub TestBasicConfig {
+	my $output;
+	my $config = "/tmp/rds_test.conf";
+	my $destination = "/tmp/archive";
+	my $source = "/tmp/source";
+
+	open (CFH, '>', $config) or die "Failed to generate test conf file";
+	print CFH "destination $destination\n";
+	print CFH "backup $source\n";
+	close(CFH);
+
+	$output = `perl $RDATESYNC $config`;
+	ok( $output =~ "destination: $destination" );
+	ok( $output =~ "backup: $source" );
+}
+
 &PrintUsageNoArgs();
+&TestBasicConfig();
--- rdatesync.pl
+++ rdatesync.pl
@@ -4,8 +4,29 @@ if ($#ARGV < 0) {
 	&usage();
 	exit 0;
 }
+&readConf($ARGV[0]);
 
 sub usage {
 	print "Usage:\n"
-		. "    rdatesync.pl [config file]\n";
+		. "    rdatesync.pl [config file]\n"
+		. "\n"
+		. "    config file must be of the format:\n"
+		. "        destination /path/to/backups/destination\n"
+		. "        backup /path/to/backup\n"
+		. "\n";
+}
+
+sub readConf {
+	my $filename = shift;
+	if (open CFH, $filename) {
+		while (<CFH>) {
+			if ($_ =~ /^destination\s+(.*)$/) {
+				print "destination: $1\n";
+			}
+			elsif ($_ =~ /^backup\s+(.*)$/) {
+				print "backup: $1\n";
+			}
+		}
+		close($cfh);
+	}
 }

f80fe2a3cd0ab3bdceebf7b816f4c85f858f6a66
Paul Walker <wpaulsg@gmail.com>
2015-12-15 23:25:01 -0600

    simplediff.sh: added simple diff tool to diff two directories

---
+++ simplediff.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Simple diff: compare two directories and report new/modified/removed for any
+# differences.
+#
+# modified files are determined by comparing the inode of each
+#
+# retults will be placed in the new directory.
+#
+
+if [ ! -d "$1" -o ! -d "$2" ]; then
+	echo "Usage:"
+	echo "    $0 old_dir new_dir"
+	exit;
+fi
+
+DIR_OLD="$(cd "$1"; pwd)";
+DIR_NEW="$(cd "$2"; pwd)";
+RESULTS_TMP="/tmp/sd.sh.tmp"
+RESULTS_FILE="$DIR_NEW/changes"
+
+inode() {
+	ls -i "$1" | awk '{print $1}'
+}
+
+compare_to_new() {
+	local path="$1";
+	if [ ! -f "$DIR_NEW/$path" ]; then
+		echo "$path -" >> $RESULTS_TMP
+	elif [ $(inode "$DIR_NEW/$path") != $(inode "$DIR_OLD/$path") ]; then
+		echo "$path ~" >> $RESULTS_TMP
+	fi
+}
+
+compare_to_old() {
+	local path="$1";
+	if [ ! -f "$DIR_OLD/$path" ]; then
+		echo "$path +" >> $RESULTS_TMP
+	fi
+	# No need to check inode again
+}
+
+rm -f $RESULTS_TMP
+
+cd "$DIR_OLD"
+find * -type f | while read file; do compare_to_new "$file"; done
+cd "$DIR_NEW"
+find * -type f | while read file; do compare_to_old "$file"; done
+
+sed -i '/^changes /d' $RESULTS_TMP
+
+echo "$(basename "$DIR_OLD") => $(basename "$DIR_NEW")" > $RESULTS_FILE
+cat $RESULTS_TMP | sort | sed 's/^\(.\+\) \(.\)$/\2 \1/' >> $RESULTS_FILE
+rm -f $RESULTS_TMP
